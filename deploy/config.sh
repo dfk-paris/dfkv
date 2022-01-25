7764a1606a0821cd1165440fbdb8653d37f6daac
Moritz Schepp <moritz.schepp@gmail.com>
2022-01-25 15:07:49 -0800

    fixes deployment

--- deploy/config.sh
+++ deploy/config.sh
@@ -5,6 +5,6 @@ export HOST="app@192.168.30.213"
 export SSH_KEY="$HOME/.ssh/id_rsa"
 export PORT="22"
 export DEPLOY_TO="/var/storage/host/dfkv-ng"
-export COMMIT="master"
+export COMMIT="ng"
 export RUBY_VERSION="2.7.3"
 export KEEP=5
