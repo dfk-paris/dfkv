df0e4e514b1ca8f19725f7045842d6baaeea7a98
Moritz Schepp <moritz.schepp@gmail.com>
2022-05-29 16:14:38 -0700

    adapts deploy config

--- deploy/config.sh
+++ deploy/config.sh
@@ -4,7 +4,15 @@ export REPO="."
 export HOST="app@192.168.30.213"
 export SSH_KEY="$HOME/.ssh/id_rsa"
 export PORT="22"
-export DEPLOY_TO="/var/storage/host/dfkv-ng"
-export COMMIT="ng"
-export RUBY_VERSION="2.7.3"
+export RUBY_VERSION="2.7.5"
 export KEEP=5
+
+function ng {
+  export DEPLOY_TO="/var/storage/host/dfkv-ng"
+  export COMMIT="ng"
+}
+
+function production {
+  export DEPLOY_TO="/var/storage/host/dfkv"
+  export COMMIT="master"
+}
